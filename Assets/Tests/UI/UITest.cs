32eeb3e88352e1fad763dfbf608ae536c5aaec0f
Loïc Teixeira <hello@loicteixeira.name>
2015-05-10 09:40:45 +1200

    Create a scene to test UI

---
+++ Assets/Tests/UI/UITest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UITest : MonoBehaviour
+{
+	public void SignInButtonClicked()
+	{
+		GJAPI.UI.Manager.Instance.ShowSignIn((bool success) => {
+			if (success)
+			{
+				Debug.Log("Logged In");
+			}
+			else
+			{
+				Debug.Log("Dismissed");
+			}
+		});
+	}
+
+	public void SignOutButtonClicked()
+	{
+		if (GJAPI.Manager.Instance.CurrentUser != null)
+		{
+			GJAPI.Manager.Instance.CurrentUser.SignOut();
+		}
+	}
+}
