18a302191be44370a2bcb2f0d92ccbae6c9b36d4
Loïc Teixeira <hello@loicteixeira.name>
2015-05-23 17:25:55 +1200

    Expose return value for dismissible windows
    Whether the windows dismiss callback should return success or failure
    depend on the window type itself.
    For example, dismissing the login window is a failure as the login was
    unsuccessful but dismissing the trophy window is a success as the window
    was shown properly.
    
    Note: The UI close button should return the same value.

--- Assets/Plugins/GameJolt/Scripts/UI/States/DismissibleWindow.cs
+++ Assets/Plugins/GameJolt/Scripts/UI/States/DismissibleWindow.cs
@@ -4,11 +4,13 @@ namespace GJAPI.UI.States
 {
 	public class DismissibleWindow : StateMachineBehaviour
 	{
+		public bool returnValue = false;
+
 		override public void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
 		{
 			if (Input.GetKeyDown(KeyCode.Escape))
 			{
-				animator.GetComponentInChildren<UI.Controllers.BaseWindow>().Dismiss(false);
+				animator.GetComponentInChildren<UI.Controllers.BaseWindow>().Dismiss(returnValue);
 			}
 		}
 	}
