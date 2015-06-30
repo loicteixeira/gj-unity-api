345e30f2f019f6fed9060ba681b1693845174450
Loïc Teixeira <hello@loicteixeira.name>
2015-06-30 20:35:01 +1200

    Add tooltips to the Settings asset
    I initially wanted a Custom Inspector but this is a much simpler solution and still works fine.
    Close #13.

--- Assets/Plugins/GameJolt/Scripts/API/Settings.cs
+++ Assets/Plugins/GameJolt/Scripts/API/Settings.cs
@@ -5,17 +5,25 @@ namespace GameJolt.API
 	[System.Serializable]
 	public class Settings : ScriptableObject {
 		[Header("Game")]
+		[Tooltip("The game ID. It can be found on the Game Jolt website under Dashboard > YOUR-GAME > Game API > API Settings.")]
 		public int gameID;
+		[Tooltip("The game Private Key. It can be found on the Game Jolt website under Dashboard > YOUR-GAME > Game API > API Settings.")]
 		public string privateKey;
 
 		[Header("Settings")]
+		[Tooltip("The time in seconds before an API call should timeout and return failure.")]
 		public float timeout = 10f;
+		[Tooltip("Automatically create and ping sessions once a user has been authentified.")]
 		public bool autoPing = true;
+		[Tooltip("Cache High Score Tables and Trophies information for faster display.")]
 		public bool useCaching = true;
 
 		[Header("Debug")]
+		[Tooltip("AutoConnect in the Editor as if the game was hosted on GameJolt.")]
 		public bool autoConnect = false;
+		[Tooltip("The username to use for AutoConnect.")]
 		public string user;
+		[Tooltip("The token to use for AutoConnect.")]
 		public string token;
 	}
 }
