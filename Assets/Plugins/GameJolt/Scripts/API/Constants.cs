4e2112094c0b416cb9166ca3387371ea02056658
Loïc Teixeira <hello@loicteixeira.name>
2015-12-09 15:04:30 +0100

    Remove URL trailing slashes
    Fix #59.

--- Assets/Plugins/GameJolt/Scripts/API/Constants.cs
+++ Assets/Plugins/GameJolt/Scripts/API/Constants.cs
@@ -20,27 +20,27 @@ namespace GameJolt.API
 		public const string API_PROTOCOL = "http://";
 		public const string API_ROOT = "gamejolt.com/api/game/";
 		public const string API_VERSION = "1";
-		public const string API_BASE_URL = API_PROTOCOL + API_ROOT + "v" + API_VERSION + "/";
+		public const string API_BASE_URL = API_PROTOCOL + API_ROOT + "v" + API_VERSION;
 
-		public const string API_USERS_AUTH = "users/auth/";
-		public const string API_USERS_FETCH = "users/";
+		public const string API_USERS_AUTH = "/users/auth";
+		public const string API_USERS_FETCH = "/users";
 
-		public const string API_SESSIONS_OPEN = "sessions/open/";
-		public const string API_SESSIONS_PING = "sessions/ping/";
-		public const string API_SESSIONS_CLOSE = "sessions/close/";
+		public const string API_SESSIONS_OPEN = "/sessions/open";
+		public const string API_SESSIONS_PING = "/sessions/ping";
+		public const string API_SESSIONS_CLOSE = "/sessions/close";
 
-		public const string API_SCORES_ADD = "scores/add/";
-		public const string API_SCORES_FETCH = "scores/";
-		public const string API_SCORES_TABLES_FETCH = "scores/tables/";
+		public const string API_SCORES_ADD = "/scores/add";
+		public const string API_SCORES_FETCH = "/scores";
+		public const string API_SCORES_TABLES_FETCH = "/scores/tables";
 
-		public const string API_TROPHIES_ADD = "trophies/add-achieved/";
-		public const string API_TROPHIES_FETCH = "trophies/";
+		public const string API_TROPHIES_ADD = "trophies/add-achieved";
+		public const string API_TROPHIES_FETCH = "/trophies";
 
-		public const string API_DATASTORE_SET = "data-store/set/";
-		public const string API_DATASTORE_UPDATE = "data-store/update/";
-		public const string API_DATASTORE_FETCH = "data-store/";
-		public const string API_DATASTORE_REMOVE = "data-store/remove/";
-		public const string API_DATASTORE_KEYS_FETCH = "data-store/get-keys/";
+		public const string API_DATASTORE_SET = "/data-store/set";
+		public const string API_DATASTORE_UPDATE = "/data-store/update";
+		public const string API_DATASTORE_FETCH = "/data-store";
+		public const string API_DATASTORE_REMOVE = "/data-store/remove";
+		public const string API_DATASTORE_KEYS_FETCH = "/data-store/get-keys";
 
 		public const string IMAGE_RESOURCE_REL_PATH = "Images/";
 
