6a1c55d4208b9dee0c74bf94e75b36d0503418f1
Loïc Teixeira <hello@lta.me>
2017-03-18 16:15:31 +1300

    Add multi-level test scene

---
+++ Assets/Tests/Load/LoadTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LoadTest : MonoBehaviour
+{
+	public void SignInButtonClicked()
+	{
+		GameJolt.UI.Manager.Instance.ShowSignIn((bool success) => {
+			if (success)
+			{
+				Debug.Log("Logged In");
+			}
+			else
+			{
+				Debug.Log("Dismissed");
+			}
+		});
+	}
+
+	public void IsSignedInButtonClicked() {
+		bool isSignedIn = GameJolt.API.Manager.Instance.CurrentUser != null;
+		if (isSignedIn) {
+			Debug.Log("Signed In");
+		}
+		else {
+			Debug.Log("Not Signed In");
+		}
+	}
+
+	public void LoadSceneButtonClicked(string sceneName) {
+		Debug.Log("Loading Scene " + sceneName);
+		Application.LoadLevel(sceneName);
+	}
+}
